3498a59d26f20de99cc766b181a85c9932dfaf74
andrew-j-yu <a.j.yu@wustl.edu>
2024-10-01 21:12:23 -0400

    update

--- HTMLExample.js
+++ HTMLExample.js
@@ -1,12 +1,6 @@
 function handleSubmit(event) {
-    event.preventDefault();  // Prevent form submission
-    
-    // Get the value from the input field
+    event.preventDefault();
     const name = document.getElementById('name').value;
-    
-    // Find the div where we want to display the name
     const resultDiv = document.getElementById('result');
-    
-    // Set the content of the div to the name entered by the user
     resultDiv.textContent = "Hello, " + name + "!";
 }
